bd91253dcdfe044f77aa0e86add0916bbe0bf9e2
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-12-13 21:10:19 -0500

    Add a prepend method to insert a node to the beginning of the linked list.

--- javascript/linkedList.js
+++ javascript/linkedList.js
@@ -3,7 +3,44 @@ class LinkedList {
         this.head = value;
         this.next = null;
     }
+
+    prepend(value) {
+        if (this.head === null) {
+            this.head = value;
+        } else {
+            const newHead = value;
+            //
+            if (this.next === null) {
+                this.tail = {
+                    value: this.head,
+                    next: null
+                }
+
+                this.next = this.tail;
+            }
+
+            else {
+                this.next = {
+                    value: this.head,
+                    next: this.next
+                }
+            }
+            //
+            this.head = newHead;
+        }
+
+        return this;
+    }
 }
 
 a = new LinkedList();
+console.log(a);
+
+a.prepend(1);
+console.log(a);
+
+a.prepend(2);
+console.log(a);
+
+a.prepend(3);
 console.log(a);
