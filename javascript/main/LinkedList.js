46b6bf5ec7ec72ca8c0ba62ed44df1aa6dcf6d09
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-12-14 19:27:02 -0500

    Add code to append to the LinkedList.

--- javascript/main/LinkedList.js
+++ javascript/main/LinkedList.js
@@ -33,4 +33,81 @@ module.exports = class LinkedList {
 
         return this;
     }
+
+    append(value) {
+        if (this.head === null) {
+            this.head = value;
+        } else {
+            const newNode = {
+                value: value,
+                next: null
+            };
+
+            if (this.next === null) {
+                this.tail = newNode;
+                this.next = this.tail;
+            } else {
+                this.tail.next = newNode;
+                this.tail = newNode;
+            }
+        }
+
+        return this;
+    }
 }
+
+// a = new LinkedList();
+// console.log(a);
+
+// a.prepend(-1);
+// console.log(a);
+
+// a.prepend(-2);
+// console.log(a);
+
+// a.prepend(-3);
+// console.log(a);
+
+// a.append(1);
+// console.log(a);
+
+// a.append(2);
+// console.log(a);
+
+// a.append(3);
+// console.log(a);
+
+// b = new LinkedList(-1);
+// console.log(b);
+
+// b.prepend(-2);
+// console.log(b);
+
+// b.prepend(-3);
+// console.log(b);
+
+// b.append(1);
+// console.log(b);
+
+// b.append(2);
+// console.log(b);
+
+// b.append(3);
+// console.log(b);
+
+// c = new LinkedList(1);
+
+// c.append(2);
+// console.log(c);
+
+// c.append(3);
+// console.log(c);
+
+// c.prepend(-1);
+// console.log(c);
+
+// c.prepend(-2);
+// console.log(c);
+
+// c.prepend(-3);
+// console.log(c);
